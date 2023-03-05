c9ef7f404c77b7d58c7811de308d3525f4fbee1f
shashankpandey6389 <123095727+shashankpandey6389@users.noreply.github.com>
2023-03-05 10:49:47 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'product',pathMatch:'full'},
+  {path:'product',component:ProductComponent},
+  {path:'cart',component:CartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
